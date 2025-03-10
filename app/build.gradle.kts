15a463495dcda50afb472d72e15400cf788ea85f
BaHe4ka666 <soulerfun@gmail.com>
2025-03-10 14:54:59 +0500

    new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
