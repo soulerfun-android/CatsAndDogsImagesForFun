fbbf6e5031612f39bc328418438e1e8d9e12658c
BaHe4ka666 <soulerfun@gmail.com>
2025-03-10 14:49:48 +0500

    main screen

--- app/src/main/java/com/example/lelele/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lelele/presentation/MainActivity.kt
@@ -1,16 +1,16 @@
-package com.example.lelele
+package com.example.lelele.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.lelele.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
 
 
 
